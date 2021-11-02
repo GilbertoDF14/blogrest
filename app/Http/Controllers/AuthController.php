64103ed250332e69700758cebbdc8a56fff1d4f5
GilbertoDF14 <48808299+GilbertoDF14@users.noreply.github.com>
2021-11-01 18:30:27 -0600

    AuthCorregido

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,5 +1,5 @@
 <?php
-
+//kZ2zswvPw5GWiu19ff6oMfITucxCGoko
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -9,22 +9,26 @@ use App\Http\Controllers\JWT;
 class AuthController extends Controller
 {
     public function login($user,$pass){
-        //$this->validate($req,[
-        //    'user'=>'required',
-        //    'pass'=>'required']);
+        // $this->validate($req, [
+        //     'user'=>'required', 
+        //     'pass'=>'required']);
+
         $result = User::find($user);
+
         if($result){
-            if(Hash::check($pass, $result->pass)){
+            if (Hash::check($pass, $result->pass)){
                 return response()->json([
                     'auth' => true,
                     'user' => $result,
                     'token' => JWT::create($result, env('JWT_SECRET', 'kZ2zswvPw5GWiu19ff6oMfITucxCGoko'))
                 ], 200);
             }else{
-                return response()->json(['status'=>'failed']),404);
+                return response()->json(['status'=>'failed'], 404);
             }
         }else{
-            return response()->json(['status'=>'failed']),404);
+            return response()->json(['status'=>'failed'], 404);
         }
+
+    
     }
 }
